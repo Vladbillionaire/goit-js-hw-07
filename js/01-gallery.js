8c9f76d6dbaf56d9074c5144ffe42f12f6992d54
Vladbillionaires <vladislav8174@gmail.com>
2023-08-06 20:53:55 +0300

    Test

---
+++ js/01-gallery.js
@@ -0,0 +1,47 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector(".gallery")
+
+const cardsMarkup = createGalleryCardMarkup(galleryItems)
+gallery.insertAdjacentHTML('beforeend', cardsMarkup)
+gallery.addEventListener('click', onGalleryElClick)
+
+function createGalleryCardMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>
+    `;
+    }).join("");
+}
+
+function onGalleryElClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+    const originalImg = e.target.dataset.source;
+    modalWindow(originalImg);
+}
+
+function modalWindow(img) {
+    const instance = basicLightbox.create(`
+    <img src="${img}" width="800" height="600">
+`)
+    instance.show()
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+            instance.close()
+        }
+    })
+}
